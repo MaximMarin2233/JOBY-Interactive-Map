c01367c4a17694608bbf3baf82c60d3cfbe10bb9
maxxxutko99 <bafldalovdursun78@gmail.com>
2023-02-08 22:51:18 +0300

    geolocation for ymaps was created

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,7 +1,10 @@
 import './components/modal';
+import './components/map';
 
 import { modal } from "./components/modal";
+import { map } from "./components/map";
 
 window.addEventListener('DOMContentLoaded', () => {
   modal();
+  map();
 });
---
+++ src/js/components/map.js
@@ -0,0 +1,41 @@
+export function map() {
+
+  function init() {
+    let map;
+
+    ymaps.geolocation.get({
+      mapStateAutoApply: true,
+      autoReverseGeocode: false
+    })
+    .then(
+      function(result) {
+        let coordinates = result.geoObjects.get(0).geometry.getCoordinates();
+
+        createMap({
+          center: coordinates,
+          zoom: 11
+        }, {
+          searchControlProvider: 'yandex#search'
+        });
+      },
+      function(err) {
+        createMap({
+          center: [55.75987793362054,37.619763925026476],
+          zoom: 11
+        });
+      }
+    );
+  }
+
+  function createMap(obj1, obj2) {
+    map = new ymaps.Map('joby-map', obj1, obj2);
+
+    map.controls.remove('trafficControl');
+    map.controls.remove('typeSelector');
+    map.controls.remove('fullscreenControl');
+    map.controls.remove('rulerControl');
+  }
+
+  ymaps.ready(init);
+
+}
