7278729d6c892bb6ed53b4e6db18aded62049b7e
maxxxutko99 <bafldalovdursun78@gmail.com>
2023-03-12 19:31:44 +0300

    markup for user was created

--- src/resources/libs/sign-in.php
+++ src/resources/libs/sign-in.php
@@ -8,12 +8,12 @@
   $mysqli = new Mysqli('localhost', 'root', '', 'joby');
   $mysqli->query("SET NAMES utf8");
   $result = $mysqli->query("SELECT email, password FROM user WHERE id > $count");
-  $signIn = 'err';
+  $signIn = false;
 
   while($row = $result->fetch_assoc()) {
       $object = json_decode(json_encode($row), FALSE);
       if($object->email == $email and $object->password == $password) {
-        $signIn = 'success';
+        $signIn = true;
       }
   }
 
