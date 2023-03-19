6e4794f08b878f581d0b55e1332e27e33a99f8cc
maxxxutko99 <bafldalovdursun78@gmail.com>
2023-03-19 03:01:42 +0300

    hash for passwords was created

--- src/resources/libs/sign-up.php
+++ src/resources/libs/sign-up.php
@@ -1,8 +1,9 @@
 <?php
   $email = $_POST['email'];
   $password = $_POST['password'];
+  $hash = password_hash($password, PASSWORD_BCRYPT);
   $mark = $_POST['mark'];
   $mysqli = new Mysqli('localhost', 'root', '', 'joby');
   $mysqli->query("SET NAMES utf8");
-  $mysqli->query("INSERT INTO `user`(`email`, `password`, `mark`) VALUES('$email', '$password', '$mark')");
+  $mysqli->query("INSERT INTO `user`(`email`, `password`, `mark`) VALUES('$email', '$hash', '$mark')");
 ?>
